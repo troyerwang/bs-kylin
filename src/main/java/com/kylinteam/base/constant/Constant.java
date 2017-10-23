f016c1e9b64f9b8a92fbe66fcb9c181f7f4c4218
Xiaolong Wang <troyer.wang@gmail.com>
2017-10-23 11:39:34 +0800

    完成entity实体的定义和优化

---
+++ src/main/java/com/kylinteam/base/constant/Constant.java
@@ -0,0 +1,15 @@
+package com.kylinteam.base.constant;
+
+public class Constant {
+
+    // DB constant
+    // Account & Password & ModelName & Short title And so on...
+    public static final Integer DB_FIELD_LENGTH_50 = 50;
+    // Short description
+    public static final Integer DB_FIELD_LENGTH_100 = 100;
+    // General description
+    public static final Integer DB_FIELD_LENGTH_300 = 300;
+    // Long description
+    public static final Integer DB_FIELD_LENGTH_1000 = 1000;
+
+}
---
+++ src/main/java/com/kylinteam/base/entity/User.java
@@ -0,0 +1,54 @@
+package com.kylinteam.base.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import com.kylinteam.base.constant.Constant;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -721912626034105086L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "account", length = Constant.DB_FIELD_LENGTH_50, nullable = false, unique = true)
+    public String account;
+    @Column(name = "password", length = Constant.DB_FIELD_LENGTH_50, nullable = false)
+    private String password;
+    @Column(name = "displayname", length = Constant.DB_FIELD_LENGTH_50)
+    private String displayName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+}
