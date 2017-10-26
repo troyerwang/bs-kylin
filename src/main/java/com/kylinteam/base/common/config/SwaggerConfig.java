f68b71ff2164389159b7efbfbd2f24d2d944fee6
Xiaolong Wang <troyer.wang@gmail.com>
2017-10-26 16:40:18 +0800

    修改Swagger配置为扫描包模式

--- src/main/java/com/kylinteam/base/common/config/SwaggerConfig.java
+++ src/main/java/com/kylinteam/base/common/config/SwaggerConfig.java
@@ -3,13 +3,11 @@ package com.kylinteam.base.common.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static com.google.common.base.Predicates.or;
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -19,16 +17,16 @@ public class SwaggerConfig {
     public Docket demoApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .paths(or(regex("/users/.*")))
+                .apis(RequestHandlerSelectors.basePackage("com.kylinteam.base.controller"))
                 .build()
                 .apiInfo(demoApiInfo());
     }
 
     private ApiInfo demoApiInfo() {
         return new ApiInfoBuilder()
-                .title("Swagger2文档生成工具测试") //大标题
-                .description("更多信息可以关注: http://swagger.io")  //详细描述
-                .version("1.0")//版本
+                .title("Swagger2文档生成工具测试")
+                .description("更多信息可以关注: http://swagger.io")
+                .version("1.0")
                 .build();
     }
 
--- src/main/java/com/kylinteam/base/controller/HelloController.java
+++ src/main/java/com/kylinteam/base/controller/HelloController.java
@@ -2,12 +2,13 @@ package com.kylinteam.base.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     public String sayHello() {
         return "Hello kylinteam!";
